d0ce466afd26bb2f8adeb798e64b67156eea2484
Kenniusboggs <ogbogukenny@yahoo.com>
2023-04-25 11:07:08 +0100

    Building the Navbar

---
+++ src/assets/index.js
@@ -0,0 +1,86 @@
+import logo from "./MaticIconSmall1.png";
+import quotes from "./quotes.svg";
+import send from "./MaticIconSmall1.png";
+import shield from "./MaticIconSmall1.png";
+import star from "./MaticIconSmall1.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./Mfacebook.svg";
+import instagram from "./Minstagram.svg";
+import linkedin from "./Mlinkedin.svg";
+import twitter from "./Mtwitter-squared.png";
+import youtube from "./Myoutube.svg";
+import people01 from "./MaticEsther2.png";
+import people02 from "./MaticOsato.png";
+import people03 from "./MaticKenny.png";
+import people04 from "./MaticBrian.png";
+import people05 from "./MaticDaniel1.png";
+import people06 from "./MaticJudith.png";
+import people07 from "./MaticAbdulmatin2.png";
+import people08 from "./MaticAdenike.png";
+import people09 from "./MaticAzeez.png";
+import people10 from "./MaticRachael.png";
+import MaticVid1 from "./MaticDriveVid1.mp4";
+import MaticVid2 from "./MaticDriveVid3.gif";
+import MaticProto from "./MaticProto.jpg";
+import contactImg from "./contactImg.svg";
+import features01 from "./FeaturesPics1.jpg";
+import features02 from "./FeaturesPics2.jpg";
+import features03 from "./FeaturesPics3.jpg";
+import features04 from "./FeaturesPics4.jpg";
+import features05 from "./FeaturesPics5.jpg";
+import features06 from "./FeaturesPics6.jpg";
+import features07 from "./FeaturesPics7.jpg";
+import partnership from "./MaticPartnership.png";
+import microsoftBanner from "./MicroSoftStartupsCelebrationBadgeLight.png";
+import googleLogo1 from "./google-logo.png";
+import googleLogo2 from "./google-logo-shady.png";
+
+
+export {
+  logo,
+  quotes,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  youtube,
+  people01,
+  people02,
+  people03,
+  people04,
+  people05,
+  people06,
+  people07,
+  people08,
+  people09,
+  people10,
+  features01,
+  features02,
+  features03,
+  features04,
+  features05,
+  features06,
+  features07,
+  MaticVid1,
+  MaticVid2,
+  MaticProto,
+  contactImg,
+  partnership,
+  microsoftBanner,
+  googleLogo1,
+  googleLogo2,
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,143 @@
-// import React from 'react'
-
+import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebaseConfig";
+import { signOut } from "firebase/auth";
+import logo from "../assets/MaticIconSmall1.png";
+import menu from "../assets/menu.svg";
+import close from "../assets/close.svg";
 
 
 function Navbar() {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-                <div className="container">
-                    <img src="./images/maticdrive-logo-ori.png" alt="brand-logo" className="brand-logo" />
-                    <a className="navbar-brand" href="index.html">Matic<b className="brand-name" style={{color: "#022fbe"}}>Drive</b></a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="oi oi-menu"></span>
-                    </button>
+    const navLinks = [
+        {
+          id: "/",
+          title: "Home",
+        },
+        {
+          id: "/aboutus",
+          title: "About Us",
+        },
+        {
+          id: "/team",
+          title: "Our Team",
+        },
+        {
+          id: "/contactus",
+          title: "Contact Us",
+        },
+        {
+          id: "/blog",
+          title: "Blogs",
+        },
+        {
+          id: "/signin",
+          title: "Sign In",
+        },
+      ];
+    const [active, setActive] = useState("Home");
+    const [toggle, setToggle] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
+  
+  
+    const [user] = useAuthState(auth);
+    
+  
+    useEffect(() => {
+      const onScroll = () => {
+        if (window.scrollY > 50) {
+          setScrolled(true);
+        } else {
+          setScrolled(false);
+        }
+      }
 
-                    <div className="collapse navbar-collapse" id="ftco-nav">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item active"><a href="index.html" className="nav-link">Home</a></li>
-                            <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
-                            <li className="nav-item"><a href="services.html" className="nav-link">Services</a></li>
-                            <li className="nav-item"><a href="blog.html" className="nav-link">Blog</a></li>
-                            <li className="nav-item"><a href="contact.html" className="nav-link">Contact</a></li>
-                        </ul>
+      window.addEventListener("scroll", onScroll);
+
+      return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+
+
+    return (
+        <nav className={`ss:w-full w-[100%] flex justify-between items-center navbar bg-primary`}>
+
+            <div>
+                <div className="flex justify-center items-center">
+                    <Link to="/"><img src={logo} alt="Maticdrive logo" className={`${scrolled ? "w-[35px] h-[35px] ss:w-[50px] ss:h-[50px] duration-1000" : ""} w-[50px] h-[50px] ss:w-[70px] ss:h-[70px] m-2`} /></Link>
+                    <div className="flex flex-col -mt-2">
+                        <div className="text-[20px] text-white">Matic <span className="text-gradient">Drive</span></div>
                     </div>
                 </div>
-            </nav>
-        </div>
+            </div>
+
+
+            <ul className="list-none md:flex hidden justify-end items-center flex-1">
+                {navLinks.map((nav) => (
+                    <li
+                        key={nav.id}
+                        className={`font-poppins font-normal cursor-pointer text-[16px] hover:border-b-4 hover:pb-2 hover:border-yellow-300 ${active === nav.title ? "text-white" : "text-gray-400"
+                            } ${user && nav.title === "Sign In" ? "hidden" : "block"} mr-6`}
+                        onClick={() => setActive(nav.title)}
+                    >
+                        <Link to={`${nav.id}`}>{nav.title}</Link>
+                    </li>
+                ))}
+                <button onClick={() => { signOut(auth) }} className={`${user ? "block" : "hidden"} w-[80px] h-[25px] opacity-60 text-[14px] text-center text-primary font-semibold pb-1 -mr-12 bg-text-gradient rounded-[7px]`}>Sign Out</button>
+            </ul>
+
+            <div className="md:hidden flex flex-1 justify-end items-center pr-4">
+
+                <img
+                    src={toggle ? close : menu}
+                    alt="menu"
+                    className="w-[22px] h-[22px] object-contain"
+                    onClick={() => setToggle(!toggle)}
+                />
+
+                <div
+                    className={`${!toggle ? "hidden" : "flex"
+                        } p-6 bg-black-gradient absolute top-20 z-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar flex-col`}
+                >
+                    <ul className="list-none flex justify-end items-start flex-1 flex-col">
+                        {navLinks.map((nav) => (
+                            <li
+                                key={nav.id}
+                                className={`font-poppins font-medium cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"
+                                    } ${user && nav.title === "Sign In" ? "hidden" : "block"} mb-4`}
+                                onClick={() => {
+                                    setActive(nav.title);
+                                    setToggle(!toggle);
+                                }}
+                            >
+                                <Link to={`${nav.id}`}>{nav.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <button onClick={() => {
+                        signOut(auth);
+                        setToggle(!toggle);
+                    }}
+                        className={`${user ? "block" : "hidden"} w-[80px] h-[30px] text-[14px] text-start text-primary font-bold pl-2 pb-1 bg-text-gradient rounded-[7px]`}>
+                        Sign Out
+                    </button>
+                </div>
+
+            </div>
+            <div className={`${user ? "block" : "hidden"} flex justify-center items-center text-primary font-bold rounded-[50%] bg-text-gradient w-[28px] h-[28px] -ml-2 -mr-3 ss:ml-14 ss:-mr-14`}>
+                {
+                    (
+                        user && user.displayName.split(" ")[0].split("")[0].toUpperCase()
+                    )
+                }
+                {
+                    (
+                        user && user.displayName.split(" ")[1].split("")[0].toUpperCase()
+                    )
+                }
+            </div>
+        </nav>
     )
 }
 
 export default Navbar
+
